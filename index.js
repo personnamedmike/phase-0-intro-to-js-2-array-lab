c2ecf4554fe5d2f6dbb465a269113ccad683404c
Mike Starr <imhs@protonmail.com>
2021-12-08 14:18:45 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const catsCopy = [...cats, name];
+  return catsCopy;
+}
+
+function prependCat(name) {
+  const prependCat = [name, ...cats];
+  return prependCat;
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
